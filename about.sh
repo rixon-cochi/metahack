08536de8a90acce61dff6bfa936c64b8abe25c8c
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-11-16 17:52:02 +0530

    Update about.sh

--- about.sh
+++ about.sh
@@ -9,14 +9,16 @@ echo -e "\e[93m [3] \e[0m\e[91m If it violates the law with this tool the risk i
 echo ""
 echo -e "\e[96m  Installing Metasploit Framework \e[0m"
 echo "" 
-sleep 5 & PID=$! #simulate a long process
+sleep 20 & PID=$! #simulate a long process
 
-echo "THIS MAY TAKE A WHILE, DOWNLOADING METASPLOIT" | lolcat
+echo "THIS MAY TAKE A WHILE, INSTALLING METASPLOIT " | lolcat
 printf "["
-# While process is running...                                                                   while kill -0 $PID 2> /dev/null; do
+# While process is running...
+while kill -0 $PID 2> /dev/null; do 
     printf  "▓"
     sleep 0.1
 done
+printf "] done!"
 clear
 echo " DOWNLOAD STARTED "
 sleep 5
