3bca89e6d0bc7c85375989639f2170047875af8b
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-11-16 17:46:14 +0530

    Update about.sh

--- about.sh
+++ about.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-clear
 echo ""
 echo ""
 echo -e "\e[96m Using And Installing This Tools That Mean You Ready For \e[0m"
@@ -9,8 +8,8 @@ echo -e "\e[93m [2] \e[0m\e[91m No Warranty \e[0m"
 echo -e "\e[93m [3] \e[0m\e[91m If it violates the law with this tool the risk is borne by the user \e[0m"
 echo ""
 echo -e "\e[96m  Installing Metasploit Framework \e[0m"
-echo ""
-sleep sleep 5 & PID=$! #simulate a long process
+echo "" 
+sleep 5 & PID=$! #simulate a long process
 
 echo "THIS MAY TAKE A WHILE, DOWNLOADING METASPLOIT" | lolcat
 printf "["
