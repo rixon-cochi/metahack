5cd42a3a865c02e1d05956099421ef4bf4ef03c3
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-11-16 17:43:34 +0530

    Create logo.sh

---
+++ logo.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# code by tech cochi
+# subscribe my channel
+clear
+
+echo -e "\e[34m  __  __ _____ _____  _ \e[0m\e[91m    _   _    _    ____ _  __ \e[0m"
+echo -e "\e[34m |  \/  | ____|_   _|/ \ \e[0m\e[91m  | | | |  / \  / ___| |/ / \e[0m"
+echo -e "\e[34m | |\/| |  _|   | | / _ \ \e[0m\e[91m | |_| | / _ \| |   | ' / \e[0m"
+echo -e "\e[34m | |  | | |___  | |/ ___ \ \e[0m\e[91m|  _  |/ ___ | |___| . \ \e[0m"
+echo -e "\e[34m |_|  |_|_____| |_/_/   \_ \e[0m\e[91m|_| |_/_/   \_\____|_|\_\ \e[0m"
+echo ""
+echo -e "CODE DEVELOPED BY \e[40;38;5;82m TECH \e[30;48;5;82m COCHI \e[0m"
