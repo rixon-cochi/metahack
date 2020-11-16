5dcea27952bec9021e7487489518ae6ec5516bd0
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-11-16 18:22:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,3 +5,12 @@
 echo ""
 bash logo.sh
 bash about.sh
+sleep 3
+bash load.sh
+echo ""
+echo ""
+sleep 3
+xdg-open https://m.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
+echo " PLEASE WAIT 20 SECOND "
+sleep 20
+bash metahack.sh
