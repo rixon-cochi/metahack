cc1ce094bd6be69c34a409d19140e34d8072434b
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2020-11-16 17:16:47 +0530

    Add files via upload

---
+++ about.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+clear
+echo ""
+echo ""
+echo -e "\e[96m Using And Installing This Tools That Mean You Ready For \e[0m"
+echo -e ""
+echo -e "\e[93m [1] \e[0m\e[91m Use At Your Own Risk \e[0m"
+echo -e "\e[93m [2] \e[0m\e[91m No Warranty \e[0m"
+echo -e "\e[93m [3] \e[0m\e[91m If it violates the law with this tool the risk is borne by the user \e[0m"
+echo ""
+echo -e "\e[96m  Installing Metasploit Framework \e[0m"
+echo ""
+sleep sleep 5 & PID=$! #simulate a long process
+
+echo "THIS MAY TAKE A WHILE, DOWNLOADING METASPLOIT" | lolcat
+printf "["
+# While process is running...                                                                   while kill -0 $PID 2> /dev/null; do
+    printf  "▓"
+    sleep 0.1
+done
+clear
+echo " DOWNLOAD STARTED "
+sleep 5
+clear
+bash metahack.sh
+
+
