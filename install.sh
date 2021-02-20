b6fbf168fe4c1a9cd03e989115f0745f5115d636
Tech cochi <61864331+rixon-cochi@users.noreply.github.com>
2021-02-20 15:17:52 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -22,8 +22,7 @@ clear
 sleep 1
 xdg-open https://www.youtube.com/channel/UCiE0p7rXWBEncUVsLo1C5Xg
 echo " wait for 15 seconds "
-sleep 15
-echo " create account on ecoin "
+echo " create account on cashkaro.com "
 echo " set your browser "
-termux-open-url http://techcoch.blogspot.com/2020/10/follow-instructions.html?m=1
+termux-open-url https://cashkaro.com?r=8026255&fname=Rixon
 echo ""
